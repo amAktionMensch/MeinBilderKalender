bece26fddb13fd590f1efb8891a861a6c0c19295
amAaktionMensch <anna.moellemann@aktion-mensch.de>
2019-02-02 14:00:52 +0100

    initial Frontend

--- app/src/main/java/com/example/meinbilderkalender/MainActivity.java
+++ app/src/main/java/com/example/meinbilderkalender/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.meinbilderkalender;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Configuration of HelpButton
+        Button btnHelp = findViewById(R.id.btnHelp);
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "42", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
